a3380a7865b8481eb51bc88e10403b752e18f208
Blake Erickson <o.blakeerickson@gmail.com>
2017-05-11 08:44:38 -0600

    finish up sso endpoint

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'roda'
 require 'base64'
 require 'openssl'
+require 'uri'
+require 'cgi'
+require 'securerandom'
 
 class App < Roda
 
@@ -8,12 +11,32 @@ class App < Roda
     r.get "sso" do
       sso = r['sso']
       sig = r['sig']
-      decoded_sso = Base64.decode64(sso)
       sig_test = OpenSSL::HMAC.hexdigest("sha256", 'abcdefghij', sso)
-      if sig_test == sig
-        "match"
+      if sig_test != sig
+        return "error"
       else
-        "not match"
+        base64_sso = URI.unescape(sso)
+        decoded_sso = Base64.decode64(base64_sso)
+        #uri = URI.parse(decoded_sso)
+        #CGI::parse(decoded_sso)
+        params = Rack::Utils.parse_nested_query(decoded_sso) #=> {"a" => "2"}
+        nonce = params['nonce']
+        return_sso_url = params['return_sso_url']
+        external_id = rand 1000
+        username = SecureRandom.hex
+        payload = {
+          "nonce" => nonce,
+          "name" => username,
+          "external_id" => external_id,
+          "email" => "#{username}@test.com",
+          "username" => username,
+          "require_activation" => "true"
+        }
+        payload = Rack::Utils.build_query(payload)
+        base64_payload = Base64.encode64(payload)
+        encoded_payload = URI.escape(base64_payload)
+        new_sig = OpenSSL::HMAC.hexdigest("sha256", 'abcdefghij', base64_payload)
+        r.redirect "http://192.168.56.2:3000/session/sso_login?sso=#{encoded_payload}&sig=#{new_sig}"
       end
     end
   end
